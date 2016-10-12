0db15dcda875624536691f2a3dbb1447d761bee6
Martin Feldsztejn <mfeldsztejn@gmail.com>
2016-10-11 21:40:34 -0300

    change where the port comes from

--- server/app.js
+++ server/app.js
@@ -16,6 +16,6 @@ router.route('/people/:id')
 
 app.use('/', router);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
